5d09e2ff691051cf7ecacaf4149b07bfaead21bc
EmmanuelAfk <eblnimchak999@outlook.com>
2025-01-13 00:17:30 +0100

    sigin

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,11 +17,25 @@ module.exports = {
         navshadow:"#9FB2FF1A",
         prices:"#F7F7F7",
         mobile:"#BBCD04",
-        hero_pattern:"linear-gradient(to top, rgba(255, 255, 255, 1), rgba(159, 178, 255, 1))"
+
       },
       backgroundImage:{
-        
-      }
+        jema:"url(../02-images/jema.png)",
+        profile:"url(../../01-pages/01-03-signin/02-images/iconamoon_profile-light.svg)",
+        hero_pattern:"linear-gradient(to top, rgba(255, 255, 255, 1), rgba(159, 178, 255, 1))",
+        password:"url(../../01-pages/01-03-signin/02-images/jam_key.svg)"
+      },
+      backgroundPosition: {
+        'left-top': 'left top',
+        'left-center': 'left center',
+        'left-bottom': 'left bottom',
+        'right-top': 'right top',
+        'right-center': 'right center',
+        'right-bottom': 'right bottom',
+        'center-top': 'center top',
+        'center-center': 'center center',
+        'center-bottom': 'center bottom',
+      },
     },
   },
   plugins: [],
