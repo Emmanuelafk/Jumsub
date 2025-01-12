27863cf45fb41f1c71b0bf632656a1675922f2dc
EmmanuelAfk <eblnimchak999@outlook.com>
2025-01-12 19:06:09 +0100

    css

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html', // Include all HTML files in the root directory
+    './**/*.html', // Include all HTML files in subdirectories
+    './11-resources/01-css/**/*.css', // Include all CSS files in the specified directory
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#001F8A",
+        gradient1:"rgba(255, 255, 255, 1)",
+        gradient2:"rgba(159, 178, 255, 1)",
+        nav1:"#FFFFFF",
+        nav2:"#F7F7F7",
+        gradient3:"#FCFCFC",
+        navshadow:"#9FB2FF1A",
+      },
+      backgroundImage:{
+        hero_pattern:"linear-gradient(to top, rgba(255, 255, 255, 1), rgba(159, 178, 255, 1))"
+      }
+    },
+  },
+  plugins: [],
+}
+
